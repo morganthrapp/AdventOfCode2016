404807adc2d8fcdc9e66b6abc806f142a24f6884
Morgan Thrapp <morgant@sca-corp.com>
2016-12-02 09:53:51 -0500

    Adding the solution for Day 1 Part 1!

---
+++ Day1/Day1a.py
@@ -0,0 +1,11 @@
+data = 'L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4'.split(',')
+heading = 1
+position = 0, 0
+movements = lambda x: {1: (x, 0), 2: (0, x), 3: (-x, 0), 4: (0, -x)}
+for (direction, *distance) in map(str.strip, data):
+    distance = int(''.join(distance))
+    heading = heading + 1 if direction == 'R' else heading - 1
+    heading = 4 if heading < 1 else 1 if heading > 4 else heading
+    position = tuple(map(sum, zip(movements(distance)[heading], position)))
+
+print(sum(map(abs, position)))
